cbac443a51f645755d122b25b0073cbb67fa2079
Dileep Kottakota <kottakotadileepkumar@gmail.com>
2022-12-15 10:58:17 +0530

    GET Call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,30 +7,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myFamilyMembers: any = [
-    {
-      'firstName': 'Dileep',
-      'lastName': 'K',
-      'age': 29,
-      'salary': 10000
-    },
-    {
-      'firstName': 'Dileep1111',
-      'lastName': 'K',
-      'age': 19,
-      'salary': 20000
-    }
-  ]
+  myFamilyMembers: any = [];
+  // [
+  //   {
+  //     'firstName': 'Dileep',
+  //     'lastName': 'K',
+  //     'age': 29,
+  //     'salary': 10000
+  //   },
+  //   {
+  //     'firstName': 'Dileep1111',
+  //     'lastName': 'K',
+  //     'age': 19,
+  //     'salary': 20000
+  //   }
+  // ]
   filteredMembers: any;
-  
 
-  constructor() {
+
+  // dependenct injection
+  constructor(private httpClient: HttpClient) {
     this.filteredMembers = this.myFamilyMembers
+  }
+
+  pageNumber = 1
+  //success and error call back functions
+  ngOnInit() {
+    this.httpClient.get('https://reqres.in/api/users?page='+ this.pageNumber).subscribe(
+      (test: any) => {
+        console.log(test);
+        this.filteredMembers = test.data;
+        console.log(test.data[0].email)
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
+  }
+
+  nextPage() {
+    this.pageNumber = this.pageNumber + 1;
+    this.httpClient.get('https://reqres.in/api/users?page=' + this.pageNumber).subscribe(
+      (test: any) => {
+        console.log(test);
+        // this.filteredMembers.push(...test.data);
+        this.filteredMembers = test.data;
+        console.log(test.data[0].email)
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
+  }
+
+  previousPage() {
+    this.pageNumber = this.pageNumber - 1;
+    this.httpClient.get('https://reqres.in/api/users?page=' + this.pageNumber).subscribe(
+      (test: any) => {
+        console.log(test);
+        // this.filteredMembers.push(...test.data);
+        this.filteredMembers = test.data;
+        console.log(test.data[0].email)
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
   }
 
   onDataEntered(obj: any) {
     this.filteredMembers.push(obj);
   }
-  
+
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { CreateFormComponent } from './create-form/create-form.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
