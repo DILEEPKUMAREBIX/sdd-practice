5c541ea5fb8d5f5ed3cb37b2e88d7dcf85c414c8
Dileep Kottakota <kottakotadileepkumar@gmail.com>
2022-12-26 11:43:39 +0530

    Update logic in CRUD

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -9,10 +9,18 @@ export class EmployeeService {
   constructor(private httpClient: HttpClient) { }
 
   getAllEmployees() {
-    return this.httpClient.get("https://crudcrud.com/api/0803de00669d45e0b4bc87fcfd700b73/employee");
+    return this.httpClient.get("https://crudcrud.com/api/55788cffe61e4f3b917c70bb89afeafb/employee");
   }
 
   deleteEmployee(empId: any) {
-    return this.httpClient.delete("https://crudcrud.com/api/0803de00669d45e0b4bc87fcfd700b73/employee/" + empId);
+    return this.httpClient.delete("https://crudcrud.com/api/55788cffe61e4f3b917c70bb89afeafb/employee/" + empId);
+  }
+
+  createEmployee(empObj: any) {
+    return this.httpClient.post("https://crudcrud.com/api/55788cffe61e4f3b917c70bb89afeafb/employee", empObj);
+  }
+
+  updateEmployee(empObj: any, id: any) {
+    return this.httpClient.put("https://crudcrud.com/api/55788cffe61e4f3b917c70bb89afeafb/employee/" + id, empObj);
   }
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -12,6 +12,11 @@ export class EmployeeComponent implements OnInit {
 
   myEmployees: any = [];
 
+  name: any ='';
+  salary: any = '';
+  age: any = '';
+  photo: any = '';
+
   ngOnInit(): void {
     this.getEmployees();
   }
@@ -36,6 +41,64 @@ export class EmployeeComponent implements OnInit {
       },
       (error: any) => {
         console.log(error);
+      }
+    )
+  }
+
+  onSubmit() {
+    let employeeObj = {
+      'name': this.name,
+      'age': this.age,
+      'salary': this.salary,
+      'photo': this.photo
+    }
+
+    this.empService.createEmployee(employeeObj).subscribe(
+      (success: any) => {
+        console.log(success);
+        this.getEmployees();
+      },
+      (error: any) => {
+
+      }
+    )
+  }
+
+  _id: any = ''
+  isEdit: boolean = false;
+  editEmployee(editItem: any) {
+    this.name = editItem.name;
+    this.age = editItem.age;
+    this.salary = editItem.salary;
+    this.photo = editItem.photo;
+    this._id = editItem._id;
+
+    this.isEdit = true;
+  }
+
+  onUpdate() {
+    let employeeObj = {
+      'name': this.name,
+      'age': this.age,
+      'salary': this.salary,
+      'photo': this.photo,
+    }
+
+    this.empService.updateEmployee(employeeObj, this._id).subscribe(
+      (success: any) => {
+        console.log(success);
+        this.getEmployees();
+
+        this.name = '';
+        this.age = '';
+        this.salary = '';
+        this.photo = '';
+        this._id = '';
+
+        this.isEdit = false;
+      },
+      (error: any) => {
+
       }
     )
   }
